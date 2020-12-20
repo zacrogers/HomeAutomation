7d2c5775537a2ed08e87ebbdcccae83f8660a497
rogerszach <z.m.rogers@gmail.com>
2020-12-21 11:04:56 +1300

    Added power node

---
+++ PowerNode/src/main.cpp
@@ -0,0 +1,18 @@
+#include "main.h"
+
+uint8_t RELAY_PIN[NUM_RELAYS] = {};
+
+const char* ssid = SSID;
+const char* password = PASSWORD;
+
+ESP8266WebServer server(80);
+
+void setup() 
+{
+
+}
+
+void loop() 
+{
+
+}
---
+++ PowerNode/src/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <DHT.h>
+
+#include "credentials.h"
+
+#define NUM_RELAYS 4
+
+#endif
