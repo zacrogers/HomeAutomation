06c085d4ca0b96f3c66f9eb8b3163094e2f815e3
Zac <z.m.rogers@gmail.com>
2021-02-05 14:13:56 +1300

    Added access feedback functions

--- MotorisedLock/src/main.cpp
+++ MotorisedLock/src/main.cpp
@@ -1,20 +1,62 @@
 #include "main.h"
 
+ESP8266WebServer server(80);
+WiFiManager wm;
+
 Servo servo;
 bool is_locked = false;
 uint8_t current_angle = MIN_ANGLE;
 
+// Tone buzzer;
+
 void setup() 
 {
+	Serial.begin(115200);
+	pinMode(RED_LED_PIN, OUTPUT);
+	pinMode(GREEN_LED_PIN, OUTPUT);
+
+	// buzzer.begin(BUZZER_PIN);
+	// buzzer.stop();
+
+	wm.setConfigPortalBlocking(false);
+	// wm.resetSettings();	// Uncomment to clear wifi credentials
+
+	WiFi.mode(WIFI_STA);
+
+    if(wm.autoConnect("SensorNodeAp"))
+	{
+		Serial.println("Connected to wifi");
+    }
+    else 
+	{
+        Serial.println("Configportal running");
+    }
+
+	if (MDNS.begin("DoorLock")) 
+	{
+		Serial.println("MDNS responder started");
+	}
+
+	server.on("/", handle_lock_state);
+	server.onNotFound(handle_not_found);
+	server.begin();
+	Serial.println("Server started");
+
 	servo.attach(SERVO_PIN);
 }
 
 void loop() 
 {
+	wm.process();
 
+	if (WiFi.status() == WL_CONNECTED) 
+	{
+		server.handleClient();
+		MDNS.update();
+	}
 }
 
-void open_lock(void)
+void lock_open(void)
 {
 	if(is_locked)
 	{
@@ -26,9 +68,9 @@ void open_lock(void)
 
 		is_locked = false;
 	}
-
 }
-void close_lock(void)
+
+void lock_close(void)
 {
 	if(!is_locked)
 	{
@@ -37,6 +79,37 @@ void close_lock(void)
 			servo.write(current_angle);
 			delay(SERVO_DELAY_TIME);                       
 		} 		
+		
 		is_locked = true;
 	}
+}
+
+void access_granted_feedback(void)
+{
+	digitalWrite(RED_LED_PIN, LOW);
+
+	for(uint8_t i = 0; i < 4; ++i)
+	{
+		digitalWrite(GREEN_LED_PIN, HIGH);
+		delay(500);
+		digitalWrite(GREEN_LED_PIN, LOW);
+		delay(500);
+	}
+
+	digitalWrite(GREEN_LED_PIN, HIGH);
+}
+
+void access_denied_feedback(void)
+{
+	digitalWrite(GREEN_LED_PIN, LOW);
+
+	for(uint8_t i = 0; i < 4; ++i)
+	{
+		digitalWrite(RED_LED_PIN, HIGH);
+		delay(500);
+		digitalWrite(RED_LED_PIN, LOW);
+		delay(500);
+	}
+
+	digitalWrite(RED_LED_PIN, HIGH);
 }
--- MotorisedLock/src/main.h
+++ MotorisedLock/src/main.h
@@ -2,26 +2,44 @@
 #define _MAIN_H
 
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ESP8266mDNS.h>
 #include <Servo.h>
 #include <Wire.h>
+// #include <Tone.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 #include <stdint.h>
 
-// Limits for servo
+// Servo defines
 #define MAX_ANGLE 180
 #define MIN_ANGLE 20
 #define SERVO_DELAY_TIME 10
-
 #define SERVO_PIN D7
 
-/* Servo Control functions */
-void open_lock(void);
-void close_lock(void);
+#define RED_LED_PIN 0
+#define GREEN_LED_PIN 0
 
-/* Key access functions */
+#define BUZZER_PIN 0
+
+/* Servo Control */
+void lock_close(void);
+void lock_open(void);
+
+/* Key access */
 void access_granted(void);
 void access_denied(void);
 uint8_t get_key_id(void);
+ 
+/* Web server */
+void handle_lock_state(void);
+void handle_not_found(void);
+
+/* User feedback */
+void access_granted_feedback(void);
+void access_denied_feedback(void);
 
 #endif
